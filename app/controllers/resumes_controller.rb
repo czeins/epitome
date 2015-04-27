783c2322cc576c99d14cd3015f0307cf1cdfb2f5
czeins <claude.zeins@gmail.com>
2015-04-27 02:07:15 -0700

    Added CRU minus D for resumes

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -2,15 +2,17 @@ class ResumesController < ApplicationController
 # before_filter :authorize
 
   def index
-    User.find(params[:id]).resumes
+    @user = User.find(params[:user_id])
+    @resumes = @user.resumes.all
   end
 
   def new
-    @resume = Resume.new
+     @user = current_user
+     @resume = @user.resumes.build
   end
 
   def create
-    @resume =  current_user.resumes.build(resume_params)
+    @resume = current_user.resumes.build(resume_params)
     if @resume.save
       redirect_to '/'
     else
@@ -19,31 +21,32 @@ class ResumesController < ApplicationController
   end
 
   def show
-    @resume = Resume(params[:resume_id])
+    @resume = Resume.find(params[:id])
   end
 
  def edit
-    # @resume = Resume.find(params[:id])
+    @user = current_user
+    @resume = @user.resumes.find(params[:id])
   end
 
   def update
-  #   @resume = Resume.find(params[:id])
-  #     if @Resume.update_attributes(resume_params)
-  #       redirect_to resumes_path
-  #     else
-  #       render "edit"
-  #     end
+    @resume = Resume.find(params[:id])
+      if @resume.update_attributes(resume_params)
+        redirect_to 'user_resumes'
+      else
+        render "edit"
+      end
   end
 
-  def destroy
-    # @resume = Resume.find(params[:id])
-    # @resume.destroy
-    # redirect_to resumes_path
-  end
+ #  def destroy
+ #    # @resume = Resume.find(params[:id])
+ #    # @resume.destroy
+ #    # redirect_to resumes_path
+ #  end
 
   private
     def resume_params
-      params.require(:resume).permit(:user, :education, :jobs, :skills, :user_id)
+      params.require(:resume).permit(:education_1, :education_1_school, :education_1_degree, :education_1_major, :education_1_minor, :education_1_duration, :education_2, :education_2_degree, :education_2_major, :education_2_minor, :education_2_duration, :education_3, :education_3_degree, :education_3_major, :education_3_minor, :education_3_duration, :experience_1, :experience_1_position, :experience_1_duration, :experience_1_description, :experience_2, :experience_2_position, :experience_2_duration, :experience_2_description, :experience_3, :experience_3_position, :experience_3_duration, :experience_3_description, :experience_4, :experience_4_position, :experience_4_duration, :experience_4_description, :skills_description)
     end
 
 end
